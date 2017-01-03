580d92c95cddc99ab7a6d32f858be78123584282
geord359 <earleychris1983@gmail.com>
2017-01-03 18:50:26 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,10 @@
 var name = "Chris Earley";
+var role = "GIS Analyst"
 $("#main").append(["Chris Earley"]);
 var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").append(formattedName);
+$("#header").append(formattedRole);
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
