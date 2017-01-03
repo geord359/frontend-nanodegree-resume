c4ba877c1ee47fdccf6d2336897e1cba8a0d51df
geord359 <earleychris1983@gmail.com>
2017-01-03 00:10:22 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,7 @@
+var name = "Chris Earley"
 $("#main").append(["Chris Earley"]);
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").append(formattedName);
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
