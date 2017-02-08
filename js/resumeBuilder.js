49d087971b30e3a3520fb97c6d7f0c53f3d2ec5f
geord359 <earleychris1983@gmail.com>
2017-02-08 22:13:49 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,6 +5,22 @@ var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedRole);
+var skillset = ["ArcGIS", "Python", "FME", "AutoCAD"]
+
+var bio {
+  name: "Chris Earley",
+  role: "GIS Analyst",
+  Contact Info: 542156,
+  Picture Url: "images/m2012.jpg",
+  Welcome Message: "Hello",
+  Skills: skillset,
+};
+  
+$("#main").append(bio)
+  
+    
+            
+  
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
